2336736facc3d343be962a4f021ac325f24540c0
mohamedsaeed253 <muhammedsaeed253@gmail.com>
2024-09-23 16:29:51 +0300

    admin dashboard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Typography from "@mui/material/Typography";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { useState } from "react";
 
-function App() {
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+export default function MiniDrawer() {
+  const [darkMode, setDarkMode] = useState(localStorage.getItem("mode"));
+  const theme = createTheme({
+    palette: {
+      mode: darkMode === "light" ? "light" : "dark",
+    },
+  });
+  const [open, setOpen] = React.useState(false);
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <Navbar // @ts-ignore
+          open={open}
+          handleDrawerOpen={handleDrawerOpen}
+          // @ts-ignore
+          setDarkMode={setDarkMode}
+        />
+        <Sidebar
+          // @ts-ignore
+          open={open}
+          handleDrawerClose={handleDrawerClose}
+        />
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <DrawerHeader />
+          <Typography sx={{ marginBottom: 2 }}>Consequat mauris .</Typography>
+        </Box>
+      </Box>
+    </ThemeProvider>
   );
 }
-
-export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,317 @@
+import Toolbar from "@mui/material/Toolbar";
+import MuiAppBar from "@mui/material/AppBar";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
+import InputBase from "@mui/material/InputBase";
+import MenuIcon from "@mui/icons-material/Menu";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import EditIcon from "@mui/icons-material/Edit";
+import ArchiveIcon from "@mui/icons-material/Archive";
+import FileCopyIcon from "@mui/icons-material/FileCopy";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
+import { alpha, styled } from "@mui/material/styles";
+import {
+  Avatar,
+  Box,
+  Divider,
+  ListItemIcon,
+  Menu,
+  MenuItem,
+  Stack,
+  useTheme,
+} from "@mui/material";
+import { useState } from "react";
+import { Logout, PersonAdd } from "@mui/icons-material";
+const drawerWidth = 240;
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(3),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
+  },
+}));
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  variants: [
+    {
+      // @ts-ignore
+      props: ({ open }) => open,
+      style: {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(["width", "margin"], {
+          easing: theme.transitions.easing.sharp,
+          duration: theme.transitions.duration.enteringScreen,
+        }),
+      },
+    },
+  ],
+}));
+
+export default function Navbar({ open, handleDrawerOpen, setDarkMode }) {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [anchorE2, setAnchorE2] = useState(null);
+  const open1 = Boolean(anchorEl);
+  const open2 = Boolean(anchorE2);
+  const handleClick1 = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose1 = () => {
+    setAnchorEl(null);
+  };
+  const handleClick2 = (event) => {
+    setAnchorE2(event.currentTarget);
+  };
+  const handleClose2 = () => {
+    setAnchorE2(null);
+  };
+  const theme = useTheme();
+  return (
+    <AppBar
+      position="fixed"
+      // @ts-ignore
+      open={open}
+    >
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          onClick={handleDrawerOpen}
+          edge="start"
+          sx={[
+            {
+              marginRight: 5,
+            },
+            open && { display: "none" },
+          ]}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Search>
+          <SearchIconWrapper>
+            <SearchIcon />
+          </SearchIconWrapper>
+          <StyledInputBase
+            placeholder="Search…"
+            inputProps={{ "aria-label": "search" }}
+          />
+        </Search>
+        <Box flexGrow={1} />
+        <Stack direction="row">
+          {theme.palette.mode === "light" ? (
+            <IconButton
+              onClick={() => {
+                localStorage.setItem(
+                  "mode",
+                  theme.palette.mode === "light" ? "dark" : "light"
+                );
+                setDarkMode("dark");
+              }}
+              color="inherit"
+            >
+              <WbSunnyOutlinedIcon />
+            </IconButton>
+          ) : (
+            <IconButton
+              onClick={() => {
+                localStorage.setItem(
+                  "mode",
+                  theme.palette.mode === "light" ? "dark" : "light"
+                );
+                setDarkMode("light");
+              }}
+              color="inherit"
+            >
+              <DarkModeOutlinedIcon />
+            </IconButton>
+          )}
+
+          <IconButton color="inherit">
+            <NotificationsOutlinedIcon />
+          </IconButton>
+          <IconButton
+            onClick={handleClick2}
+            aria-controls={open ? "account-menu1" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            color="inherit"
+          >
+            <SettingsOutlinedIcon />
+          </IconButton>
+          <Menu
+            anchorOrigin={{
+              vertical: 52,
+              horizontal: 1470,
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            // @ts-ignore
+            anchorE2={anchorE2}
+            id="account-menu2"
+            open={open2}
+            onClose={handleClose2}
+            onClick={handleClose2}
+            slotProps={{
+              paper: {
+                elevation: 0,
+                sx: {
+                  overflow: "visible",
+                  filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                  mt: 1.5,
+                  "& .MuiAvatar-root": {
+                    width: 32,
+                    height: 32,
+                    ml: -0.5,
+                    mr: 1,
+                  },
+                  "&::before": {
+                    content: '""',
+                    display: "block",
+                    position: "absolute",
+                    top: -10,
+                    right: 14,
+                    width: 10,
+                    height: 10,
+                    bgcolor: "background.paper",
+                    transform: "translateY(50%) rotate(45deg)",
+                    zIndex: 2,
+                  },
+                },
+              },
+            }}
+          >
+            <MenuItem onClick={handleClose2}>
+              <EditIcon />
+              Edit
+            </MenuItem>
+            <MenuItem onClick={handleClose2}>
+              <FileCopyIcon />
+              Duplicate
+            </MenuItem>
+            <Divider sx={{ my: 0.5 }} />
+            <MenuItem onClick={handleClose2} disableRipple>
+              <ArchiveIcon />
+              Archive
+            </MenuItem>
+            <MenuItem onClick={handleClose2} disableRipple>
+              <MoreHorizIcon />
+              More
+            </MenuItem>
+          </Menu>
+          <IconButton
+            onClick={handleClick1}
+            aria-controls={open ? "account-menu1" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            color="inherit"
+          >
+            <PersonOutlineOutlinedIcon />
+          </IconButton>
+          <Menu
+            anchorEl={anchorEl}
+            id="account-menu1"
+            open={open1}
+            onClose={handleClose1}
+            onClick={handleClose1}
+            slotProps={{
+              paper: {
+                elevation: 0,
+                sx: {
+                  overflow: "visible",
+                  filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                  mt: 1.5,
+                  "& .MuiAvatar-root": {
+                    width: 32,
+                    height: 32,
+                    ml: -0.5,
+                    mr: 1,
+                  },
+                  "&::before": {
+                    content: '""',
+                    display: "block",
+                    position: "absolute",
+                    top: 0,
+                    right: 14,
+                    width: 10,
+                    height: 10,
+                    bgcolor: "background.paper",
+                    transform: "translateY(-50%) rotate(45deg)",
+                    zIndex: 0,
+                  },
+                },
+              },
+            }}
+            transformOrigin={{ horizontal: "right", vertical: "top" }}
+            anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+          >
+            <MenuItem onClick={handleClose1}>
+              <Avatar /> Profile
+            </MenuItem>
+            <MenuItem onClick={handleClose1}>
+              <Avatar /> My account
+            </MenuItem>
+            <Divider />
+            <MenuItem onClick={handleClose1}>
+              <ListItemIcon>
+                <PersonAdd fontSize="small" />
+              </ListItemIcon>
+              Add another account
+            </MenuItem>
+            <MenuItem onClick={handleClose1}>
+              <ListItemIcon>
+                <Logout fontSize="small" />
+              </ListItemIcon>
+              Logout
+            </MenuItem>
+          </Menu>
+        </Stack>
+      </Toolbar>
+    </AppBar>
+  );
+}
